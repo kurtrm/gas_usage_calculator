242793ddabdef608fdd0b1a9098f56f79ef20b9b
Kurt Maurer <kurtrm@gmail.com>
2018-10-17 13:13:04 -0700

    Refactored to have a third function that does all the aggregate work.

--- get_gas_prices.py
+++ get_gas_prices.py
@@ -19,26 +19,24 @@ def retrieve_gas_prices(zipcode: int) -> str:
         raise RequestException(f'Error: Received connection status {response.status_code}')
 
 
-def parse_html(zipcode: int=98105) -> str:
+def parse_html(html) -> str:
     """
     """
     prices_element = 'span[class*="styles__price"]'
     station_name_element = 'h3[class*="styles__stationNameHeader"]'
     station_address_element = 'div[class*="styles__address"]'
-    fuel_prices_html = retrieve_gas_prices(zipcode)
-    soup = BeautifulSoup(fuel_prices_html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
     prices = [element.text for element in soup.select(prices_element)]
     names = [element.text for element in soup.select(station_name_element)]
     addresses = [element.text for element in soup.select(station_address_element)]
 
-    # import pdb; pdb.set_trace()
+    return names, addresses, prices
 
+
+def get_gas_information(zipcode: int=98105) -> str:
+    """
+    """
+    fuel_prices_html = retrieve_gas_prices(zipcode)
+    names, addresses, prices = parse_html(fuel_prices_html)
     return [{'name': name, 'price': price, 'address': address}
             for price, name, address in zip(prices, names, addresses)]
-
-
-
-
-
-    
-
